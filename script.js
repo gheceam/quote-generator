e63b5c7b67a1965faccf7014306a2127f77aac35
Martin G <gheceam@san.rr.com>
2020-10-17 15:53:51 -0700

    added methods to update quote, update interface, add callback function to New Quote button

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 // Get Quote From API
 async function getQuote(){
+    const proxyURL = 'https://afternoon-cliffs-29592.herokuapp.com/'
     const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
     try {
-
+        const response = await fetch(proxyURL + apiURL);
+        const data = await response.json();
+        // console.log(data);
+        updatePageQuote(data.quoteText, data.quoteAuthor);
     }catch (error){
         console.log('whoops, no quote', error);
     }
     
 }
 
+const updatePageQuote = (quoteText, quoteAuthor) =>{
+    // console.log(quoteText,quoteAuthor);
+    const author = quoteAuthor;
+    const quote = quoteText;
+
+    document.querySelector("#quote").innerHTML = quoteText;
+    document.querySelector("#author").innerHTML = quoteAuthor;
+
+}
+
+// Initialize variables
+const button = document.querySelector("#new-quote");
+const twitterButton = document.querySelector("twitter-button");
+
+// initialize callback function to button
+button.addEventListener("click",getQuote);
 // On Load
 getQuote();
+
